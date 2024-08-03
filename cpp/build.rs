3174355bbb5ddb6696a05115df24a7c417e02821
tusooa <tusooa@kazv.moe>
2024-08-02 23:01:25 -0400

    Rebuild library if any source file has changed

--- cpp/build.rs
+++ cpp/build.rs
@@ -2,5 +2,20 @@ use cxx_build::CFG;
 
 fn main() {
     CFG.include_prefix = "vodozemac";
+
+    let sources = [
+        "build.rs",
+        "src/lib.rs",
+        "src/account.rs",
+        "src/group_sessions.rs",
+        "src/sas.rs",
+        "src/session.rs",
+        "src/types.rs",
+    ];
+
+    for source in sources {
+        println!("cargo::rerun-if-changed={}", source);
+    }
+
     cxx_build::bridge("src/lib.rs").compile("vodozemac");
 }
